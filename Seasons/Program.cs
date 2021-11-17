a33f7881d4e9b4ec3a081e72eea0d5a2c7e40973
Opilane TTHK <opilane@tthk.ee>
2021-11-17 10:19:38 +0200

    BlackSheep

---
+++ Seasons/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+namespace Seasons
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Loo programm, mis loeks winter.txt ja autumn.txt failide sisu maha, ja siis kirjutaks talvekuude loetelu winter.txt 
+            //failisse ja sügiskuude loetelu autumn.txt failisse.
+
+            string autumnFile = @"C:\Users\opilane\samples\autumn.txt";
+            string winterFile = @"C:\Users\opilane\samples\winter.txt";
+
+            string[] winterData = File.ReadAllLines(autumnFile);
+            string[] autumnData = File.ReadAllLines(winterFile);
+
+            File.WriteAllLines(autumnFile, autumnData);
+            File.WriteAllLines(winterFile, winterData);
+        }
+    }
+}
